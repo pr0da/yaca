327442097ddcaf1a696e4caef3b72aa4fc8460ad
David Prokaj <prokajdavid@gmail.com>
2020-10-05 19:24:43 +0200

    build: setup environment variables

---
+++ app.config.ts
@@ -0,0 +1,9 @@
+import 'dotenv/config';
+import { ExpoConfig, ConfigContext } from '@expo/config';
+
+export default ({ config }: ConfigContext): Partial<ExpoConfig> => ({
+  ...config,
+  extra: {
+    githubClientId: process.env.REACT_NATIVE_GITHUB_CLIENT_ID,
+  },
+});
